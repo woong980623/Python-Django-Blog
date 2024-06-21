189e58325cce779b37831a3954fd711b9bfc29e3
woong980623 <urmyver@naver.com>
2024-06-21 18:15:40 +0900

    Delete blog directory
    업데이트

--- blog/blog1/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import post_list
-
-urlpatterns = [
-    path('', post_list, name='post_list'),
-]
--- blog/blog1/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog1/post_list.html', {'posts': posts})
