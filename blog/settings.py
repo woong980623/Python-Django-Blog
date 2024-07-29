e718f3ecc43135106325a42884dee7023fb8217b
woong980623 <urmyver@naver.com>
2024-07-29 10:36:19 +0900

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--1mx1xki-e=wqhz!8+solraefws(am8gpajgn$vbug7hl(t#%x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.209.10.200']
 
 
 # Application definition
